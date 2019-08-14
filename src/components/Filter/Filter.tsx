9aa2e013b1d9aaef00b931addc78bbd91f858db5
Yury Lavrukhin <yurka7321@gmail.com>
2019-08-14 19:13:50 +0300

    Add stops filters logic with local state

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,29 +1,94 @@
 import React from 'react'
+import { StopFiltersState } from '../StopsFilters/StopsFilters'
 
 import css from './Filter.module.styl'
 
 interface FilterProps {
-    label: string
+    text: string
     checkboxId: string
-    checked?: boolean
+    checked: boolean
+    setFiltersState: React.Dispatch<React.SetStateAction<StopFiltersState>>
 }
 
+type stopFiltersIds = [
+    'allStops',
+    'noStops',
+    'oneStop',
+    'twoStops',
+    'threeStops',
+]
+
 export const Filter = (props: FilterProps) => {
-    const { checked, label, checkboxId } = props
+    const { checked, text, checkboxId } = props
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.persist()
+
+        props.setFiltersState((prevState) => {
+            if (event.target.id === 'allStops') {
+                const result = {} as StopFiltersState
+                const stopFilterStateKeys = Object.keys(
+                    prevState,
+                ) as stopFiltersIds
+
+                stopFilterStateKeys.forEach((key: keyof StopFiltersState) => {
+                    result[key] = event.target.checked
+                })
+
+                return { ...result }
+            }
+
+            if (event.target.checked === false) {
+                return {
+                    ...prevState,
+                    [event.target.id]: event.target.checked,
+                    allStops: false,
+                }
+            }
+
+            if (event.target.checked === true) {
+                const prevCheckboxValues = Object.values(prevState).splice(1)
+                let unCheckedCounters = 0
+
+                for (let i = 0; i < prevCheckboxValues.length; i++) {
+                    if (prevCheckboxValues[i] === false) unCheckedCounters++
+
+                    if (unCheckedCounters > 1) {
+                        return {
+                            ...prevState,
+                            [event.target.id]: event.target.checked,
+                            allStops: false,
+                        }
+                    }
+
+                    if (i === prevCheckboxValues.length - 1) {
+                        return {
+                            ...prevState,
+                            [event.target.id]: event.target.checked,
+                            allStops: true,
+                        }
+                    }
+                }
+            }
+
+            return { ...prevState }
+        })
+    }
 
     return (
         <div className={css.Filter}>
             <label htmlFor={checkboxId} className={css.Filter_Label}>
                 <span>
                     <input
-                        defaultChecked={checked}
+                        checked={checked}
                         className={css.Filter_Input}
-                        type="checkbox"
+                        type='checkbox'
                         id={checkboxId}
+                        onChange={handleChange}
                     />
                     <span className={css.Filter_CustomCheckBox}></span>
                 </span>
-                {label}
+                {text}
             </label>
         </div>
     )
