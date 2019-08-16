9cb06e9fcfffe30d65e3edceaabecc155567fcb8
uqe <arturkkos@gmail.com>
2019-08-17 02:26:39 +0300

    fuck difficult shit

--- src/components/StopsFilters/StopsFilters.tsx
+++ src/components/StopsFilters/StopsFilters.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { Filter, StopFiltersKeys } from '../Filter/Filter'
+import React, { useState } from 'react'
+import { Filter, StopFiltersIds } from '../Filter/Filter'
 import { updateFiltersAction } from '../../redux/store/filters/stops/actions'
 
 import css from './StopsFilters.module.styl'
@@ -17,75 +17,53 @@ export interface StopFiltersDispatchProps {
 }
 
 export const StopsFilters = (props: StopFiltersDispatchProps) => {
-    const initialState = {
+    const [stopFilters, setStopFilter] = useState({
         allStops: true,
         stops_0: true,
         stops_1: true,
         stops_2: true,
         stops_3: true,
+    })
+
+    const changeStopFilterValue = (key: StopFiltersIds) => {
+        setStopFilter((prevState) => ({
+            ...prevState,
+            [key]: !prevState[key],
+        }))
     }
-
-    const [filtersManager, setFiltersState] = useState(initialState)
-
-    useEffect(() => {
-        const updateFiltersPayload = { ...filtersManager }
-
-        delete updateFiltersPayload.allStops
-
-        const updateFiltersPayloadKeys = Object.keys(
-            updateFiltersPayload,
-        ) as StopFiltersKeys
-
-        updateFiltersPayloadKeys.forEach((key) => {
-            if (updateFiltersPayload[key] === true) {
-                delete updateFiltersPayload[key]
-            }
-
-            if (updateFiltersPayload[key] === false) {
-                updateFiltersPayload[key] = true
-
-                const keyId = key.slice(-1)
-                delete Object.assign(updateFiltersPayload, {
-                    [keyId]: updateFiltersPayload[key],
-                })[key]
-            }
-        })
-
-        props.updateFilters(updateFiltersPayload)
-    }, [filtersManager])
 
     return (
         <>
             <p className={css.StopsFilters_Title}>Количество пересадок</p>
             <Filter
-                text='Все'
-                checkboxId='allStops'
-                checked={filtersManager.allStops}
-                setFiltersState={setFiltersState}
+                text="Все"
+                checkboxId="allStops"
+                checked={stopFilters.allStops}
+                setFiltersState={() => changeStopFilterValue('allStops')}
             />
             <Filter
-                text='Без пересадок'
-                checkboxId='stops_0'
-                checked={filtersManager.stops_0}
-                setFiltersState={setFiltersState}
+                text="Без пересадок"
+                checkboxId="stops_0"
+                checked={stopFilters.stops_0}
+                setFiltersState={() => changeStopFilterValue('stops_0')}
             />
             <Filter
-                text='1 пересадка'
-                checkboxId='stops_1'
-                checked={filtersManager.stops_1}
-                setFiltersState={setFiltersState}
+                text="1 пересадка"
+                checkboxId="stops_1"
+                checked={stopFilters.stops_1}
+                setFiltersState={() => changeStopFilterValue('stops_1')}
             />
             <Filter
-                text='2 пересадки'
-                checkboxId='stops_2'
-                checked={filtersManager.stops_2}
-                setFiltersState={setFiltersState}
+                text="2 пересадки"
+                checkboxId="stops_2"
+                checked={stopFilters.stops_2}
+                setFiltersState={() => changeStopFilterValue('stops_2')}
             />
             <Filter
-                text='3 пересадки'
-                checkboxId='stops_3'
-                checked={filtersManager.stops_3}
-                setFiltersState={setFiltersState}
+                text="3 пересадки"
+                checkboxId="stops_3"
+                checked={stopFilters.stops_3}
+                setFiltersState={() => changeStopFilterValue('stops_3')}
             />
         </>
     )
